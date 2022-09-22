004b7c6300139272aae61a2defb7997d8d238d1d
vishal kalola <vkalola@een.com>
2022-09-22 20:31:07 +0200

    Initial Commit

---
+++ Flickr/FlickrApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrApp.swift
+//  Flickr
+//
+//  Created by vishal on 9/22/22.
+//
+
+import SwiftUI
+
+@main
+struct FlickrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
